8fc04866a507cbd63a8206d232f73d788166992b
Philip Haitzer <philip@wundergraben.de>
2021-11-03 22:06:41 +0100

    Created ParticleContainer class and implemented functionality (Thomas+Philip)

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -1,42 +1,17 @@
-
+/**
+ * @brief Main class for Simulation
+ */
 #include "FileReader.h"
 #include "outputWriter/XYZWriter.h"
 #include "utils/ArrayUtils.h"
+#include "ParticleContainer.h"
 
 #include <iostream>
 #include <list>
 #include <outputWriter/VTKWriter.h>
 
-/**** forward declaration of the calculation functions ****/
-
 /**
- * calculate the force for all particles
- */
-void calculateF();
-
-void calculateFslower();
-
-/**
- * calculate the position for all particles
- */
-void calculateX();
-
-/**
- * calculate the position for all particles
- */
-void calculateV();
-
-/**
- * plot the particles to a xyz-file
- */
-void plotParticles(int iteration);
-
-std::array<double, 3>
-calculateFij(double& m, double& m1, const std::array<double, 3> &array, const std::array<double, 3> &array1);
-bool testOptimizedFormula();
-
-/**
- * @brief shows guidelines for correct programme calls
+ * @brief shows guidelines for correct program calls
  */
 void show_help();
 
@@ -73,6 +48,7 @@ int main(int argc, char *argsv[]) {
     // prints help in case of wrong call
     bool endReset = false;
     bool deltaReset = false;
+    bool runTest = false;
     for (int i = 2; i < argc; i++) {
         std::string temp = argsv[i];
         std::string next = argsv[i+1];
@@ -80,15 +56,15 @@ int main(int argc, char *argsv[]) {
             show_help();
             return 0;
         } else if (temp == "-t_end") {
-            if (endReset || next == "-t_end" || next == "-delta_t" || atof(next.c_str()) <= 0){
+            if (endReset || next == "-t_end" || next == "-delta_t" || strtod(next.c_str(), nullptr) <= 0){
                 show_help();
                 return 0;
             }
             temp = argsv[++i];
-            end_time = atof(temp.c_str());
+            end_time = strtod(temp.c_str(), nullptr);
             endReset = true;
         } else if (temp == "-delta_t") {
-            if (deltaReset || next == "-t_end" || next == "-delta_t" || atof(next.c_str()) <= 0){
+            if (deltaReset || next == "-t_end" || next == "-delta_t" || strtod(next.c_str(), nullptr) <= 0){
                 show_help();
                 return 0;
             }
@@ -102,24 +78,32 @@ int main(int argc, char *argsv[]) {
     }
 
     FileReader fileReader;
-    fileReader.readFile(particles, argsv[1]);
+    fileReader.readFile(particleContainer.getParticles(), argsv[1]);
 
-    //testOptimizedFormula();
+    if (runTest){
+        bool success = particleContainer.testOptimizedFormula();
+        if(success){
+            std::cout << "No errors in fast force calculation\n";
+        }else{
+            std::cout << "Errors in fast force calculation\n";
+        }
+        return 0;
+    }
     double current_time = start_time;
 
     int iteration = 0;
     // for this loop, we assume: current x, current f and current v are known
     while (current_time < end_time) {
         // calculate new x
-        calculateX();
+        particleContainer.calculateX(delta_t);
         // calculate new f
-        calculateF();
+        particleContainer.calculateF();
         // calculate new v
-        calculateV();
+        particleContainer.calculateV(delta_t);
 
         iteration++;
         if (iteration % 10 == 0) {
-            plotParticles(iteration);
+            particleContainer.plotParticles(iteration);
         }
         std::cout << "Iteration " << iteration << " finished." << std::endl;
 
@@ -137,117 +121,4 @@ void show_help() {
     std::cout << "-t_end : end value, defaults to 1000" << std::endl;
     std::cout << "-delta_t : stepsize, defaults to 0.014" << std::endl;
     std::cout << "Example : ./MolSim ../eingabe-sonne.txt -t_end 1000 -delta_t 0.014" << std::endl;
-}
-/**
- * @brief calculates the force for every particle
- */
-void calculateF() {
-    //std::list<Particle>::iterator iterator;
-    //iterator = particles.begin();
-    std::vector<std::array<double, 3>> savedFijs(particles.size()-1*(particles.size())/2);
-    int currentIndexInSavedFijs = 0;
-    int indexForFji;
-    for (int i=0; i<particles.size(); i++) {
-        std::array<double, 3> fNew{};
-        //f1,1
-        //f1,2
-        //f1,3
-        for (int j=0; j<particles.size(); j++) {
-            std::array<double, 3> fij{};
-            //use the stored Fij
-            if(j < i){
-                indexForFji = (j*particles.size()+i-(j+1)*(j+2)/2);
-                fNew = fNew + -1* savedFijs[indexForFji];
-            }
-            else if (!(particles[i] == particles[j])) {
-                double normalizedDistance = ArrayUtils::L2Norm(particles[i].getX() - particles[j].getX());
-                double scalar = particles[i].getM() * particles[j].getM() / pow(normalizedDistance, 3);
-                fij = scalar * (particles[j].getX() - particles[i].getX());
-                fNew = fNew + fij;
-                //save fij if it could later be used
-                if(j > i){
-                    savedFijs.insert(savedFijs.begin()+currentIndexInSavedFijs, fij);;
-                    currentIndexInSavedFijs++;
-                }
-            }
-        }
-        particles[i].setOldF(particles[i].getF());
-        particles[i].setF(fNew);
-    }
-}
-/**
- * @brief calculates the force for every particle in an easy but inefficient way
- */
-void calculateFslower() {
-    std::list<Particle>::iterator iterator;
-    particles.begin();
-
-    for (auto &p1 : particles) {
-        std::array<double, 3> fNew{};
-        for (auto &p2 : particles) {
-            if (!(p1 == p2)) {
-                double normalizedDistance{ArrayUtils::L2Norm(p1.getX() - p2.getX())};
-                double scalar{p1.getM() * p2.getM() / pow(normalizedDistance, 3)};
-                fNew = fNew + scalar * (p2.getX() - p1.getX());
-            }
-        }
-        p1.setOldF(p1.getF());
-        p1.setF(fNew);
-    }
-}
-
-/**
- * @brief tests if calculateFslower and calculateF are equal
- */
-bool testOptimizedFormula(){
-    bool isCorrect = true;
-    for (int i = 0; i < 1000; ++i) {
-        //Store the original configuration
-        std::vector<Particle> particlesOriginal = particles;
-        //calculate the new force on particles with optimized Method
-        calculateF();
-        //store result for comparison with slower implementation
-        std::vector<Particle> resultFast = particles;
-        //reset particles to original state
-        particles = particlesOriginal;
-        calculateFslower();
-        //Now compare particles with resultFast
-        for (int j = 0; j < particles.size(); ++j) {
-            if (!(particles[j] == resultFast[j])) {
-                std::cout << "Error in Fast Calculation\n";
-                isCorrect = false;
-            }
-        }
-        calculateX();
-        calculateV();
-    }
-    return isCorrect;
-}
-
-void calculateX() {
-    for (auto &p: particles) {
-        p.setX(p.getX() + delta_t * p.getV() + pow(delta_t, 2) / (2 * p.getM()) * p.getF());
-
-    }
-}
-
-void calculateV() {
-    for (auto &p: particles) {
-        p.setV(p.getV() + (delta_t / (2 * p.getM())) * (p.getOldF() + p.getF()));
-    }
-}
-/**
- * @brief Writes the actual information of the particles into a vtk file
- *
- * @param iteration the iteration in which the information of the particles were saved
- */
-void plotParticles(int iteration) {
-    std::string out_name("MD_vtk");
-
-    outputWriter::VTKWriter writer;
-    writer.initializeOutput(particles.size());
-    for (Particle &particle : particles) {
-        writer.plotParticle(particle);
-    }
-    writer.writeFile(out_name, iteration);
 }
--- src/ParticleContainer.h
+++ src/ParticleContainer.h
@@ -2,7 +2,38 @@
 #define PSEMOLDYN_GROUPG_PARTICLECONTAINER_H
 
 
+#include <vector>
+#include <list>
+#include "Particle.h"
+
 class ParticleContainer {
+public:
+    std::vector<Particle> &getParticles();
+/**
+ * plot the particles to a xyz-file
+ */
+    void plotParticles(int &iteration);
+/**
+ * calculate the force for all particles
+ */
+    void calculateF();
+
+    void calculateFslower();
+
+/**
+ * calculate the position for all particles
+ */
+    void calculateX(const double &delta_t);
+
+/**
+ * calculate the position for all particles
+ */
+    void calculateV(const double &delta_t);
+
+    bool testOptimizedFormula();
+
+private:
+    std::vector<Particle> particles;
 
 };
 
