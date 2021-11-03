4404fb83420a09da10b88f024db9539adb5f8670
Philip Haitzer <philip@wundergraben.de>
2021-11-03 11:35:08 +0100

    Added custom cmake target for Doxygen

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,5 @@
 # make doc_doxygen optional if someone does not have / like doxygen
-
-# TODO: create CMake build option for the target.
-
-# TODO: Add a custom target for building the documentation.
+find_package(Doxygen)
+set(DOXYGEN_USE_MDFILE_AS_MAINPAGE README.md)
+set(DOXYGEN_OUTPUT_DIRECTORY ../doxys_documentation)
+doxygen_add_docs(doc_doxygen src README.md)
