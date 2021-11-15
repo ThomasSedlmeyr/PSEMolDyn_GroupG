49703e54ecd8c4f735ed4c2cc8b24d614e5efb0e
Philip Haitzer <philip@wundergraben.de>
2021-11-15 20:03:34 +0100

    Fixed problem with arrayutils and logger and changed outputs from std::cout to logger

--- src/Tests/Tests.cpp
+++ src/Tests/Tests.cpp
@@ -7,6 +7,8 @@
 #include <string>
 #include <tuple>
 #include <InputReader/XYZReader.h>
+#include <gtest/gtest.h>
+#include <SimulationLogic/Cuboid.h>
 
 
 bool Tests::testForce() {
@@ -15,3 +17,10 @@ bool Tests::testForce() {
     std::vector<std::tuple<std::string,std::array<double, 3>>> molecules = reader.getMolecules();
 }
 
+TEST(Tests, CuboidTest){
+    Cuboid cuboid = Cuboid(0, 1, 1);
+    std::array<double, 3> structure = {40, 16, 8};
+    cuboid.setStructure(structure);
+    cuboid.generateParticles(0);
+    EXPECT_EQ(cuboid.getParticles().size(), structure[0]*structure[1]*structure[2]);
+}
