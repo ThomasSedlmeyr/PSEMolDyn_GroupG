7380fe26962b857201f02c3fd408ba438a84af1f
daniel <daniel.dieburg@outlook.de>
2021-12-06 20:35:19 +0100

    first try for input parsing, driver_input.cpp only with comment

---
+++ src/XML_Parser/driver_input.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "input.h"
+
+using namespace std;
+
+int
+main ()
+{
+    try {
+        /*
+        std::unique_ptr<input> in(input("input.xml"));
+
+        // Iterate over individual person records.
+        //
+        people_t::person_sequence &ps(ppl->person());
+
+        for (people_t::person_iterator i(ps.begin()); i != ps.end(); ++i) {
+            person_t &p(*i);
+
+            // Print names: first-name and last-name are required elements,
+            // middle-name is optional.
+            //
+            cout << "name:   " << p.first_name() << " ";
+
+            if (p.middle_name().present())
+                cout << p.middle_name().get() << " ";
+
+            cout << p.last_name() << endl;
+
+            // Print gender, age, and id which are all required.
+            //
+            cout << "gender: " << p.gender() << endl
+                 << "age:    " << p.age() << endl
+                 << "id:     " << p.id() << endl
+                 << endl;
+        }
+        */
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << endl;
+    }
+}
