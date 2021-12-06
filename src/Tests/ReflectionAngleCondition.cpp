fc7274d85586d113910583421c95ac23158c8f9c
ThomasSedlmeyr <thomas.sedlmeyr@online.de>
2021-12-06 17:12:01 +0100

    Fixed all bugs in the ReflectionConditionTest.cpp

--- src/Tests/ReflectionAngleCondition.cpp
+++ src/Tests/ReflectionAngleCondition.cpp
@@ -9,6 +9,7 @@
 #include "outputWriter/XYZWriter.h"
 #include "outputWriter/VTKWriter.h"
 
+//https://stackoverflow.com/questions/28768359/comparison-of-floating-point-arrays-using-google-test-and-google-mock
 #define EXPECT_FLOATS_NEARLY_EQ(expected, actual, thresh) \
         EXPECT_EQ(expected.size(), actual.size()) << "Array sizes differ.";\
         for (size_t idx = 0; idx < std::min(expected.size(), actual.size()); ++idx) \
@@ -17,55 +18,59 @@
         }
 
 
-TEST(Tests, ReflectionAnglConditionTest) {
+TEST(Tests, ReflectionAngleConditionTest) {
     auto ljS = LennardJonesSimulation();
     std::array<int, 6> threes = {3, 3, 3, 3, 3, 3};
-    //TODO warum darf ich hier nicht {5,5,5} einstellen???
-    ParticleContainerLinkedCells particleContainer(5, 5, 5, 3, threes);
-    std::array<double, 3> fives = {5, 5, 5};
+
+    double domainSizeXYZ = 16;
+    ParticleContainerLinkedCells particleContainer(domainSizeXYZ, domainSizeXYZ, domainSizeXYZ, 3, threes);
+    std::array<double, 3> arrDomainSize = {domainSizeXYZ, domainSizeXYZ, domainSizeXYZ};
     std::unique_ptr<BoundaryConditionContainer> boundaryContainer = std::make_unique<BoundaryConditionContainer>(threes,
                                                                                                                  ParticleContainerLinkedCells::boundaryCells,
                                                                                                                  ParticleContainerLinkedCells::haloCells,
                                                                                                                  ParticleContainerLinkedCells::numberCellsX,
                                                                                                                  ParticleContainerLinkedCells::numberCellsY,
                                                                                                                  ParticleContainerLinkedCells::numberCellsZ,
-                                                                                                                 fives);
+                                                                                                                 arrDomainSize);
     //In left halo cell
     Particle p1 = Particle({-1.23, 0, 0}, {-10, 2, 1}, 1, -1, 0);
     particleContainer.addParticleToContainer(p1);
     //In right halo cell
-    Particle p2 = Particle({6, 0, 0}, {4, 2, 1}, 1, -1, 1);
+    Particle p2 = Particle({domainSizeXYZ + 2, 0, 0}, {4, 2, 1}, 1, -1, 1);
     particleContainer.addParticleToContainer(p2);
     //In top halo cell
-    Particle p3 = Particle({3, 7, 0}, {-10, 2, 1}, 1, -1, 2);
+    Particle p3 = Particle({3, domainSizeXYZ + 1.7, 0}, {10, 2, 1}, 1, -1, 2);
     particleContainer.addParticleToContainer(p3);
     //In bottom halo cell
-    Particle p4 = Particle({0, -1, 0}, {-10, 2, 1}, 1, -1, 3);
+    Particle p4 = Particle({0, -1, 0}, {10, -2, 1}, 1, -1, 3);
     particleContainer.addParticleToContainer(p4);
     //In front halo cell
-    Particle p5 = Particle({3, 0, 6}, {-10, 2, 1}, 1, -1, 4);
+    Particle p5 = Particle({3, 0, domainSizeXYZ + 2}, {10, 2, 1}, 1, -1, 4);
     particleContainer.addParticleToContainer(p5);
     //In back halo cell
-    Particle p6 = Particle({-1.23, 0, -1.5}, {-10, 2, 1}, 1, -1, 5);
+    Particle p6 = Particle({1.23, 0, -1.5}, {10, 2, -1}, 1, -1, 5);
     particleContainer.addParticleToContainer(p6);
     //Edge Case:
     //In left and front halo cell
-    Particle p7 = Particle({-1.5, 6, 0}, {-10, 2, 1}, 1, -1, 6);
+    Particle p7 = Particle({-1.5, 0, domainSizeXYZ + 1.5}, {-10, 2, 1}, 1, -1, 6);
     particleContainer.addParticleToContainer(p7);
 
+    boundaryContainer->calculateBoundaryConditions();
     std::vector<Particle> particles = particleContainer.getParticles();
     sort(particles.begin(), particles.end(), [](const Particle &p1, const Particle &p2) {
         return p1.getId() < p2.getId();
     });
 
-    boundaryContainer->calculateBoundaryConditions();
     std::array<std::array<double, 3>, 7> correctPositions = {
-            {{1.23, 0, 0}, {4, 0, 0}, {3, 3, 0}, {0, 1, 0}, {3, 0, 4}, {-1.23, 0, 1.5}, {1.5, 4, 0}}};
+            {{1.23, 0, 0}, {domainSizeXYZ - 2, 0, 0}, {3, domainSizeXYZ - 1.7, 0}, {0, 1, 0}, {3, 0, domainSizeXYZ - 2},
+             {1.23, 0, 1.5}, {1.5, 0, domainSizeXYZ - 1.5}}};
+    std::array<std::array<double, 3>, 7> correctVelocities = {
+            {{10, 2, 1}, {-4, 2, 1}, {10, -2, 1}, {10, 2, 1}, {10, 2, -1}, {10, 2, 1}, {10, 2, -1}}};
 
     for (int i = 0; i < 7; i++) {
-        EXPECT_FLOATS_NEARLY_EQ(correctPositions[i], particleContainer.getParticles()[i].getX(), 0.0000001);
+        std::cout << i << std::endl;
+        EXPECT_FLOATS_NEARLY_EQ(correctPositions[i], particles[i].getX(), 0.0000001);
+        EXPECT_FLOATS_NEARLY_EQ(correctVelocities[i], particles[i].getV(), 0.0000001);
     }
-
-
 }
 
--- src/Tests/ReflectionConditionTest.cpp
+++ src/Tests/ReflectionConditionTest.cpp
@@ -10,6 +10,7 @@
 #include "outputWriter/VTKWriter.h"
 #include "SimulationLogic/ReflectingCondition.h"
 
+//Code adapted from: https://stackoverflow.com/questions/28768359/comparison-of-floating-point-arrays-using-google-test-and-google-mock
 #define EXPECT_FLOATS_NEARLY_EQ(expected, actual, thresh) \
         EXPECT_EQ(expected.size(), actual.size()) << "Array sizes differ.";\
         for (size_t idx = 0; idx < std::min(expected.size(), actual.size()); ++idx) \
