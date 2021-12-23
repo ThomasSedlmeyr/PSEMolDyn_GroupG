34f5ed7855267d42c1acfa0a6a1d85132b47a81d
Philip Haitzer <philip@wundergraben.de>
2021-12-23 19:59:26 +0100

    Small performance improvement

--- src/Visitors/VelCalculationVisitor.cpp
+++ src/Visitors/VelCalculationVisitor.cpp
@@ -7,7 +7,7 @@
 VelCalculationVisitor::VelCalculationVisitor(double deltaT) : delta_t(deltaT) {}
 
 void VelCalculationVisitor::visitParticle(Particle &p) {
-    p.getVRef() = p.getV() + delta_t / (2 * p.getM()) * (p.getOldF() + p.getF());
+    p.setV(p.getV() + delta_t / (2 * p.getM()) * (p.getOldF() + p.getF()));
 }
 
 void VelCalculationVisitor::setDeltaT(double deltaT) {
